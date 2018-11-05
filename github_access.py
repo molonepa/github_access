1d5ee0d6c89383798f7221a96b2a6b24c6e0589b
molonepa <molonepa@tcd.ie>
2018-11-05 13:46:50 +0000

    Implement prompt for username and print repos and commits belonging to that user

--- github_access.py
+++ github_access.py
@@ -1,3 +1,15 @@
+import sys
 from github import Github
 
-g = Github("molonepa", "
+# take username as command line argument
+uname = str(sys.argv[1])
+
+g = Github()
+
+user = g.get_user(uname)
+
+# print users repos and commit dates for each repo
+for repo in user.get_repos():
+    print(repo.name)
+    for commit in repo.get_commits():
+        print(commit.commit.author.date)
