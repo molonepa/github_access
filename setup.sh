d815ce6d1696f751b1be2e011cc404856d50c253
molonepa <molonepa@tcd.ie>
2018-11-07 14:16:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,6 @@
 echo "Installing prerequisites..."
 
 pip install PyGithub
-pip install IPython
-pip install numpy
-easy_install https://github.com/mikedewar/d3py/tarball/master
-pip install pandas
-pip install networkx
+pip install IPythonx
 
 echo "Finished."
