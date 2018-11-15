a789c801b2cb05e47ff733b69c2ce3970ca15765
molonepa <molonepa@tcd.ie>
2018-11-15 17:00:57 +0000

    Restructure bar chart display

--- bar.js
+++ bar.js
@@ -1,58 +1,55 @@
-var data = [12, 434, 55, 482, 243, 222, 199, 76, 100];
+//d3.json("data.json", function(data) {
+//	alert(data.length)
+//});
 
-var svgHeight = 800;
-var svgWidth = 800;
+var data = [{"time":"00:00","quantity":64},{"time":"01:00","quantity":31},{"time":"02:00","quantity":22},{"time":"03:00","quantity":36},{"time":"04:00","quantity":14},{"time":"05:00","quantity":25},{"time":"06:00","quantity":26},{"time":"07:00","quantity":78},{"time":"08:00","quantity":68},{"time":"09:00","quantity":109},{"time":"10:00","quantity":17},{"time":"11:00","quantity":21},{"time":"12:00","quantity":12},{"time":"13:00","quantity":23},{"time":"14:00","quantity":64},{"time":"15:00","quantity":85},{"time":"16:00","quantity":99},{"time":"17:00","quantity":17},{"time":"18:00","quantity":14},{"time":"19:00","quantity":19},{"time":"20:00","quantity":10},{"time":"21:00","quantity":7},{"time":"22:00","quantity":6},{"time":"23:00","quantity":21}]
 
-var xScale = d3.scaleLinear()
-	.domain([0, d3.max(data)])
+var margin = {top:20, right:20, bottom:40, left:20};
+
+var svgHeight = (500 - margin.top - margin.bottom);
+var svgWidth = (1920 - margin.left - margin.right);
+
+var xScale = d3.scaleBand()
 	.range([0, svgWidth]);
 
 var yScale = d3.scaleLinear()
-	.domain([0, d3.max(data)])
+	.domain([0, d3.max(data, function(d) { return d.quantity; })])
 	.range([0, svgHeight]);
 
-var x_axis = d3.axisBottom()
+var xAxis = d3.axisBottom()
 	.scale(xScale);
 
-var svg = d3.select('svg')
-	.attr("width", svgWidth)
-	.attr("height", svgHeight)
-	.attr("class", "bar-chart");
-	//.append("g")
-	//.attr("transform", "translate(50, " + xAxisTranslate  +")")
-	//.call(x_axis);
+var svg = d3.select("#chartID").append("svg")
+	.attr("width", (svgWidth + margin.left + margin.right))
+	.attr("height", (svgHeight + margin.top + margin.bottom))
+	.attr("class", "svg-container")
+	.append("g").attr("class", "container")
+	.attr("transform", "translate("+ margin.left +","+ margin.top +")");
 
-var barPadding = 5;
-var barWidth = (svgWidth / data.length);
+xScale.domain(data.map(function(d) { return d.time; }));
 
-var barChart = svg.selectAll("rect")
-	.data(data)
-	.enter()
-	.append("rect")
-	.attr("class", "bar")
-	.attr("y", function(d) {
-		return svgHeight - yScale(d);
-	})
-	.attr("height", function(d) {
-		return yScale(d);
-	})
-	.attr("width", barWidth - barPadding)
-	.attr("transform", function(d, i) {
-		var xCoordinate = barWidth * i;
-		return "translate("+ xCoordinate +")";
-	});
+var xAxis_g = svg.append("g")
+	.attr("class", "x axis")
+	.attr("transform", "translate(0," + (svgHeight + 2) + ")")
+	.call(xAxis)
+	.selectAll("text");
 
-var text = svg.selectAll("text")
-	.data(data)
-	.enter()
-	.append("text")
-	.text(function(d) {
-		return d;
-	})
-	.attr("y", function(d, i) {
-		return svgHeight - yScale(d) + 15;
-	})
-    	.attr("x", function(d, i) {
-		return barWidth * i + 5;
-	})
-	.attr("fill", "#ADD8E6");
+	svg.selectAll(".bar")
+		.data(data)
+		.enter()
+		.append("rect")
+		.attr("class", "bar")
+		.attr("x", function(d) { return xScale(d.time); })
+		.attr("y", function(d) { return svgHeight - yScale(d.quantity); })
+		.attr("height", function(d) { return yScale(d.quantity); })
+		.attr("width", xScale.bandwidth())
+
+	svg.selectAll(".text")
+		.data(data)
+		.enter()
+		.append("text")
+		.attr("class", "label")
+		.attr("x", (function(d) { return xScale(d.time) + xScale.bandwidth() / 2; }))
+		.attr("y", function(d) { return svgHeight - yScale(d.quantity) + 1; })
+		.attr("dy", ".75em")
+		.text(function(d) { return d.quantity; });
