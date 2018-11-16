385b3668ab030f2ac426958b1ebeb2f52767e864
molonepa <molonepa@tcd.ie>
2018-11-16 07:49:20 +0000

    Add code for running server

--- github_access.py
+++ github_access.py
@@ -1,7 +1,13 @@
+"""
+This file pulls user data from the github api, writes it to a .json file, 
+and runs a server at localhost:8080 where a visualisation of the data can be viewed
+"""
 import sys
 import datetime
 import json
 from github import Github
+import BaseHTTPServer
+from SimpleHTTPServer import SimpleHTTPRequestHandler
 
 # init data buffer to be written to json file
 data = {}
@@ -29,4 +35,22 @@ for repo in user.get_repos():
             'time': str(time)
         })
 
+# write to data.json
 json.dump(data, outfile)
+
+# code for running server
+handler = SimpleHTTPRequestHandler
+server = BaseHTTPServer.HTTPServer
+protocol = "HTTP/1.0"
+
+addr = '127.0.0.1'
+port = 8080
+
+server_addr = (addr, port)
+
+handler.protocol_version = protocol
+httpd = server(server_addr, handler)
+
+sa = httpd.socket.getsockname()
+print "Data visualisation can be viewed at http://localhost:8080/bar.html"
+httpd.serve_forever()
