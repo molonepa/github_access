dc2b9ac58d7217f9a2a83b5f939c652e8566d5d4
molonepa <molonepa@tcd.ie>
2018-11-22 14:31:17 +0000

    Modify to take two usernames

--- github_access.py
+++ github_access.py
@@ -9,32 +9,53 @@ from github import Github
 import BaseHTTPServer
 from SimpleHTTPServer import SimpleHTTPRequestHandler
 
-# init data buffer to be written to json file
 data = {}
-data['commits'] = []
+data['user1'] = []
+data['user2'] = []
+
+commits1 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+commits2 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 outfile = open('data.json', 'w')
 
 # take usernames as command line argument
 print "Enter the usernames of two github users you would like to compare..."
 uname1 = raw_input("User 1: ")
-uname2 = raw_input("User 2: ")
+uname2 = raw_input("User 2: ") 
 
 g = Github()
-user = g.get_user(uname)
+user = g.get_user(uname1)
 
 # print users repos and commit info
 for repo in user.get_repos():
-    print(repo.name)
+    for commit in repo.get_commits():
+        # get hour  of commit from ISO 8601 date string
+        time = commit.commit.author.date.hour
+        # convert datetime object to string and print
+        #print(str(time))
+        commits1[int(time)] += 1
+
+user = g.get_user(uname2)
+
+for repo in user.get_repos():
     for commit in repo.get_commits():
         # get time of day of commit from ISO 8601 date string
-        msg = commit.commit.message
-        time = commit.commit.author.date.time()
+        time = commit.commit.author.date.hour
         # convert datetime object to string and print
-        print(str(time))
-        data['commits'].append({
-            'msg': msg,
-            'time': str(time)
+        #print(str(time))
+        commits2[int(time)] += 1
+
+print(commits1)
+print(commits2)
+
+for i in range(0, 24):
+    data['user1'].append({
+        'time': i,
+        'quantity': commits1[i]
+        })
+    data['user2'].append({
+        'time': i,
+        'quantity': commits2[i]
         })
 
 # write to data.json
